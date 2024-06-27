6af35503cfc8c8b9f7d7d635f6662531cac94eae
AnuKonne <anuradha.k9482@gmail.com>
2024-06-27 16:24:26 +0530

    istio task update

--- istio-version/background.sh
+++ istio-version/background.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 curl -L https://istio.io/downloadIstio | sh -
-export ISTIO_DIR=$(find ./istio-* -maxdepth 1 -type d -print -quit)
+sleep 5
+ISTIO_DIR=
+for dir in ./istio-*; do
+  if [ -d "$dir" ]; then
+    ISTIO_DIR="$dir"
+    break
+  fi
+done
 cd "$ISTIO_DIR"
 export PATH=$PWD/bin:$PATH
 istioctl install --set profile=demo -y
